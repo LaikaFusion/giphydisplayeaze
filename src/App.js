83a547707a2914f7768806db010a4a9f2f405a2b
LaikaFusion <newrad4@gmail.com>
2019-03-27 14:36:58 -0400

    intial test image displayed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import sampleData from './testdata';
 
 class App extends Component {
   render() {
+    const data = sampleData.data;
     return (
       <div className="App">
-        Test
+        {data.map(e=>{
+          // return e.images.fixed_height_still.url
+          return (<img src={e.images.fixed_height.url}/>)
+        })}
       </div>
     );
   }
