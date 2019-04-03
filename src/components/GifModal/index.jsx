d0f0da834f105bf691702109b540d7ee38e3ef2d
LaikaFusion <newrad4@gmail.com>
2019-04-03 17:04:24 -0400

    added copy to clip board feature to gifmodal

--- src/components/GifModal/index.jsx
+++ src/components/GifModal/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState, useEffect} from "react";
 import "./GifModal.css";
 
 //giphy will no always give a display name if there is no extra formatting over the normal username. Not all gifs have a user object
@@ -12,9 +12,32 @@ const determineName = (userObj)=>{
   return userObj.username
 }
 
+
 const index = ({ modalObj }) => {
+
+  const textAreaRef = useRef(null);
+  const [copyText, setCopyText] = useState('');
+
+
+  const copyToClipboard =(e)=> {
+    const curUrl = copyText;
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    e.target.focus();
+    setCopyText('Copied!');
+    //to make sure it reverts
+    setTimeout(()=>{ setCopyText(curUrl) }, 2000);
+  };
+  
+  //this is to intially set the copyText correctly
+  useEffect(() => {
+    setCopyText(info.embed)
+  },[]);
+
+
   const info = modalObj.info;
   let username = determineName(info.user);
+  
   return (
     <div className="GifModal">
       <a href={info.directLink}>
@@ -23,7 +46,14 @@ const index = ({ modalObj }) => {
 
       <div className="modalTextArea">{info.title.toUpperCase()}<br/>
       {info.user ? `User: ${username}`: ""}
-      
+      <br/>
+        <textarea
+          className="copyTextArea"
+          onClick={copyToClipboard}
+          readOnly
+          ref={textAreaRef}
+          value={copyText}
+        />
       </div>
     </div>
   );
