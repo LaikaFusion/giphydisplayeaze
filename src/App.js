60e5b0036bb6685b7d57c2491eedb25cc53c9195
LaikaFusion <newrad4@gmail.com>
2019-04-03 14:37:22 -0400

    tweak to Gifpage to enhance readability and useContext test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import GifPage from './components/GifPage';
-//this is temporary data  to use for testing to avoid having to use teh api repeatedly
-import sampleData from './testdata';
+import React, { useContext } from "react";
+import "./App.css";
+import GifPage from "./components/GifPage";
+//this is temporary data  to use for testing to avoid having to use the api repeatedly
+import sampleData from "./testdata";
 
-class App extends Component {
-  render() {
-    const data = sampleData.data;
-    return (
-      <div className="App">
-        <GifPage items={data} />
-      </div>
-    );
-  }
-}
+const GifModalContext = React.createContext({display:false})
+
+const App = () => {
+  const gifModal = useContext(GifModalContext);
+  const data = sampleData.data;
+  return (
+    <div className="App">
+      <GifPage items={data} />
+      {gifModal.display ? "A":"B"}
+    </div>
+  );
+};
 
 export default App;
--- src/components/GifPage/index.jsx
+++ src/components/GifPage/index.jsx
@@ -5,15 +5,14 @@ const index = ({ items }) => {
     <div className="gifpage">
       {items.map((e, i) => {
         //detects if the images heigh it bigger then it's width to allow more consistent spacing. IF the document is bellow 450px it makes the images take up more room
+        let imgSrc = e.images.fixed_width_downsampled.url;
         if (
           e.images.fixed_width_downsampled.height > 200 ||
           document.documentElement.clientWidth < 450
         ) {
-          return (
-            <img alt={e.slug} key={e.slug} src={e.images.fixed_height_downsampled.url} />
-          );
+          imgSrc = e.images.fixed_height_downsampled.url;
         }
-        return <img alt={e.slug} key={e.slug} src={e.images.fixed_width_downsampled.url} />;
+        return <img alt={e.slug} key={e.slug} src={imgSrc} />;
       })}
     </div>
   );
