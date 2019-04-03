902365d59883e0be5118633cd05b5156edd72c4c
LaikaFusion <newrad4@gmail.com>
2019-04-03 14:19:35 -0400

    added comment to gifpage

--- src/components/GifPage/index.jsx
+++ src/components/GifPage/index.jsx
@@ -4,9 +4,10 @@ const index = ({ items }) => {
   return (
     <div className="gifpage">
       {items.map((e, i) => {
+        //detects if the images heigh it bigger then it's width to allow more consistent spacing. IF the document is bellow 450px it makes the images take up more room
         if (
-          e.images.fixed_width_downsampled.innerWidth > 200 ||
-          document.documentElement.clientWidth < 500
+          e.images.fixed_width_downsampled.height > 200 ||
+          document.documentElement.clientWidth < 450
         ) {
           return (
             <img alt={e.slug} key={e.slug} src={e.images.fixed_height_downsampled.url} />
