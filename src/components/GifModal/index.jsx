b0e3e1eb9a0639a9a933670a0cfeb2c203caa307
LaikaFusion <newrad4@gmail.com>
2019-04-03 17:08:56 -0400

    added blurred background to gif modal

--- src/components/GifModal/index.jsx
+++ src/components/GifModal/index.jsx
@@ -1,59 +1,60 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./GifModal.css";
 
 //giphy will no always give a display name if there is no extra formatting over the normal username. Not all gifs have a user object
-const determineName = (userObj)=>{
-  if(!userObj ){
-    return ""
+const determineName = userObj => {
+  if (!userObj) {
+    return "";
   }
-  if(userObj.display_name ){
-    return userObj.display_name
+  if (userObj.display_name) {
+    return userObj.display_name;
   }
-  return userObj.username
-}
-
+  return userObj.username;
+};
 
 const index = ({ modalObj }) => {
-
   const textAreaRef = useRef(null);
-  const [copyText, setCopyText] = useState('');
+  const [copyText, setCopyText] = useState("");
 
-
-  const copyToClipboard =(e)=> {
+  const copyToClipboard = e => {
     const curUrl = copyText;
     textAreaRef.current.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     e.target.focus();
-    setCopyText('Copied!');
+    setCopyText("Copied!");
     //to make sure it reverts
-    setTimeout(()=>{ setCopyText(curUrl) }, 2000);
+    setTimeout(() => {
+      setCopyText(curUrl);
+    }, 2000);
   };
-  
+
   //this is to intially set the copyText correctly
   useEffect(() => {
-    setCopyText(info.embed)
-  },[]);
-
+    setCopyText(info.embed);
+  }, []);
 
   const info = modalObj.info;
   let username = determineName(info.user);
-  
-  return (
-    <div className="GifModal">
-      <a href={info.directLink}>
-        <img alt={info.title} key={info.title} src={info.imgUrl} />
-      </a>
 
-      <div className="modalTextArea">{info.title.toUpperCase()}<br/>
-      {info.user ? `User: ${username}`: ""}
-      <br/>
-        <textarea
-          className="copyTextArea"
-          onClick={copyToClipboard}
-          readOnly
-          ref={textAreaRef}
-          value={copyText}
-        />
+  return (
+    <div className="GifModalBackground">
+      <div className="gifModalCard">
+        <a href={info.directLink}>
+          <img alt={info.title} key={info.title} src={info.imgUrl} />
+        </a>
+        <div className="modalTextArea">
+          {info.title.toUpperCase()}
+          <br />
+          {info.user ? `User: ${username}` : ""}
+          <br />
+          <textarea
+            className="copyTextArea"
+            onClick={copyToClipboard}
+            readOnly
+            ref={textAreaRef}
+            value={copyText}
+          />
+        </div>
       </div>
     </div>
   );
