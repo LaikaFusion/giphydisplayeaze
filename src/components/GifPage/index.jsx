591bc8f1b0b5163fdf73d44330103db88afe9bcb
LaikaFusion <newrad4@gmail.com>
2019-04-03 13:55:36 -0400

    tweak to make sizing more consistent

--- src/components/GifPage/index.jsx
+++ src/components/GifPage/index.jsx
@@ -4,7 +4,10 @@ const index = ({ items }) => {
   return (
     <div className="gifpage">
       {items.map((e, i) => {
-        return <img alt={e.slug} src={e.images.fixed_width.url} />;
+        if(e.images.fixed_width_downsampled.height > 200){
+          return <img alt={e.slug} src={e.images.fixed_height_downsampled.url} />;
+        }
+        return <img alt={e.slug} src={e.images.fixed_width_downsampled.url} />;
       })}
     </div>
   );
