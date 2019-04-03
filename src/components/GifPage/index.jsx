4c4a7cdad8c0c56f14262058b64a43696e15d34b
LaikaFusion <newrad4@gmail.com>
2019-04-03 14:15:44 -0400

    added width detection to determine fixed width/height

--- src/components/GifPage/index.jsx
+++ src/components/GifPage/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import './GifPage.css';
+import "./GifPage.css";
 const index = ({ items }) => {
   return (
     <div className="gifpage">
       {items.map((e, i) => {
-        if(e.images.fixed_width_downsampled.height > 200){
-          return <img alt={e.slug} src={e.images.fixed_height_downsampled.url} />;
+        if (
+          e.images.fixed_width_downsampled.innerWidth > 200 ||
+          document.documentElement.clientWidth < 500
+        ) {
+          return (
+            <img alt={e.slug} key={e.slug} src={e.images.fixed_height_downsampled.url} />
+          );
         }
-        return <img alt={e.slug} src={e.images.fixed_width_downsampled.url} />;
+        return <img alt={e.slug} key={e.slug} src={e.images.fixed_width_downsampled.url} />;
       })}
     </div>
   );
