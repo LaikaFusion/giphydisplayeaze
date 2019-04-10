d3daec793ffe7e7e21464590ee2be6f7028197d2
LaikaFusion <newrad4@gmail.com>
2019-04-10 18:00:47 -0400

    api.js linted

--- src/api.js
+++ src/api.js
@@ -4,23 +4,26 @@ const key = process.env.REACT_APP_GIPHY_KEY;
 const responseAmount = 25;
 let curOffset = 0;
 
-
-const request = (gifsRating = "G")=>{
-  return axios.get('https://api.giphy.com/v1/gifs/trending', {
-    params: {
-      api_key: key,
-      rating: gifsRating,
-      limit: responseAmount,
-      offset: curOffset
-    }
-  })
-  .then(function ({data}) {
-    curOffset = curOffset + responseAmount;
-    return data.data;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+const request = (gifsRating = 'G') => {
+  return (
+    axios
+      .get('https://api.giphy.com/v1/gifs/trending', {
+        params: {
+          api_key: key,
+          rating: gifsRating,
+          limit: responseAmount,
+          offset: curOffset,
+        },
+      })
+      // this does need to be destructured to allow iteration
+      .then(({ data }) => {
+        curOffset += responseAmount;
+        return data.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  );
+};
 
 export default request;
