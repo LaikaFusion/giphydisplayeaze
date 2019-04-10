f54b680d019b16be8b12574205e8a7ccca783e29
LaikaFusion <newrad4@gmail.com>
2019-04-10 17:18:27 -0400

    added proptypes and lint fixes to GifLoader component

--- src/components/GifLoader/index.jsx
+++ src/components/GifLoader/index.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
-import LoadingAnimation from "../LoadingAnimation";
-const index = ({ slug, imgSrc, onclickFunc = ()=>{} }) => {
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import LoadingAnimation from '../LoadingAnimation';
+
+const index = ({ slug, imgSrc, onclickFunc = () => {} }) => {
   const [Loaded, setLoaded] = useState(false);
 
   return (
-    <div className="gifContainer" onClick={onclickFunc}>
+    <div
+      role="button"
+      className="gifContainer"
+      onClick={onclickFunc}
+      onKeyUp={onclickFunc}
+      tabIndex={0}
+    >
       {Loaded ? null : <LoadingAnimation />}
       <img
-        style={Loaded ? {width:"100%"} : { display: "none" }}
+        style={Loaded ? { width: '100%' } : { display: 'none' }}
         alt={slug}
         key={slug}
         src={imgSrc}
@@ -16,5 +25,12 @@ const index = ({ slug, imgSrc, onclickFunc = ()=>{} }) => {
     </div>
   );
 };
-
+index.defaultProps = {
+  slug: 'Missing Slug',
+};
+index.propTypes = {
+  slug: PropTypes.string,
+  imgSrc: PropTypes.string.isRequired,
+  onclickFunc: PropTypes.func.isRequired,
+};
 export default index;
